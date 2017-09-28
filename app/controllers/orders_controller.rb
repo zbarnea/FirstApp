886976abda973ec3caf861ad6d491142315a4e2f
Ionut Zbarnea <zbarnea.ionut@gmail.com>
2017-09-28 16:29:05 +0100

    4.3 Orders resources created

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < Application_controller.rb
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,7 @@ class StaticPagesController < ApplicationController
   end
   def contact
   end
+  def landing_page
+    @featured_product = Product.first
+  end
 end
