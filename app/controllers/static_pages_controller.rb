a380172000d0b5340a6e0fbcfc90d10b6c7ce10d
Ionut Zbarnea <zbarnea.ionut@gmail.com>
2017-11-03 16:32:43 +0000

    4.6 Deleted the migration color, improved products page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
   def about
   end
+
   def contact
   end
+  
   def landing_page
     @products = Product.limit(3)
   end
--- db/migrate/20170926224151_rename_colors_in_products.rb
+++
@@ -1,5 +0,0 @@
-class RenameColorsInProducts < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :products, :colour, :color
-  end
-end
