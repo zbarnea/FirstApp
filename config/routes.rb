1d41beb9886e298e5a109dfd920297e77e2f5932
Ionut Zbarnea <zbarnea.ionut@gmail.com>
2017-11-08 00:16:10 +0000

    5.1 Update site root

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
-  root 'static_pages#landing_page'
+  get 'static_pages/index'
+
+  get 'static_pages/landing_page'
+
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
