f2972dcdac580006e380f6820a30d37477e4b76d
Ionut Zbarnea <zbarnea.ionut@gmail.com>
2017-11-30 17:02:31 +0000

    6.14 Improved app performance with ActiveJob and ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,6 @@ class Comment < ApplicationRecord
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
 
+  after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
+
 end
